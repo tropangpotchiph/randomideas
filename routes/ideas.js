7261541b1f3371b92f621b73bed2189db99801f7
Ryan Azur <kodegowd64@gmail.com>
2023-05-22 10:54:06 +0800

    .

--- routes/ideas.js
+++ routes/ideas.js
@@ -2,30 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Idea = require('../models/idea');
 
-const ideas = [
-  {
-    id: 1,
-    text: "Growth is painful. Change is painful. But nothing is as painful asstaying stuck somewhere you don't belong",
-    tag: 'Agriculture',
-    username: 'Superman',
-    date: '2017-01-02',
-  },
-  {
-    id: 2,
-    text: '“If you have good habits, time becomes your ally. All you need is patience',
-    tag: 'Science',
-    username: 'Ironman',
-    date: '2019-03-05',
-  },
-  {
-    id: 3,
-    text: 'Within you is a stillness and a sanctuary to which you can retreat at any time and be yourself',
-    tag: 'Physics',
-    username: 'Victor Wenbanyama',
-    date: '2013-04-08',
-  },
-];
-
 //get all ideas
 router.get('/', async (req, res) => {
   try {
