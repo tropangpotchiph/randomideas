df3e50f5b6bfa312d7d756867d240e62e60e0f57
Ryan Azur <kodegowd64@gmail.com>
2023-05-23 10:16:27 +0800

    07forms

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,10 @@
 import Modal from './components/modal';
+import IdeaForm from './components/IdeaForm';
+import IdeaList from './components/IdeaList';
 import './css/style.css';
 
 const modal = new Modal();
+const ideaForm = new IdeaForm();
+ideaForm.render();
+const ideaList = new IdeaList();
+ideaList.render();
