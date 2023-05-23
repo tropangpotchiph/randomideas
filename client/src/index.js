e63953567e9a56b6f25d117fddb6223782a0fcca
Ryan Azur <kodegowd64@gmail.com>
2023-05-23 09:27:02 +0800

    06modal

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,4 @@
+import Modal from './components/modal';
 import './css/style.css';
 
-const modal = document.querySelector('#modal');
-const modalBtn = document.querySelector('#modal-btn');
-
-function open() {
-  modal.style.display = 'block';
-}
-
-function close() {
-  modal.style.display = 'none';
-}
-
-function outsideClick(e) {
-  if (e.target === modal) {
-    close();
-  }
-}
-
-modalBtn.addEventListener('click', open);
-window.addEventListener('click', outsideClick);
+const modal = new Modal();
